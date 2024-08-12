8df41ec7e5f67a0a2a012d3dd0fa2f006275cc4f
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-12 16:28:00 +0200

    Add skeleton navigatio bar

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NavBarComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
--- src/app/home/nav-bar/nav-bar.component.ts
+++ src/app/home/nav-bar/nav-bar.component.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+  ],
   templateUrl: `./nav-bar.component.html`,
   styleUrl: './nav-bar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
