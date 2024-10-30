d2c456c0197df86550f4633d9ad1cd0427f35a49
francois0002 <francois.gosselin.lille@gmail.com>
2024-10-30 15:05:59 +0100

    capacitor

---
+++ android/app/src/main/java/com/lamatennis/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lamatennis.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lamatennis.app',
+  appName: 'lama-tennis',
+  webDir: 'dist/lama-tennis/browser'
+};
+
+export default config;
