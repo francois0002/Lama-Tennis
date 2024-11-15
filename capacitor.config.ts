5f2bdf0531634ad985c1174af5db314b987a059f
François <francois.gosselin.lille@gmail.com>
2024-11-15 17:51:26 +0100

    Add Readme

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.lamatennis.app',
-  appName: 'lama-tennis',
+  appName: 'Lama Tennis',
   webDir: 'dist/lama-tennis/browser'
 };
 
